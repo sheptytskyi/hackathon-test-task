57180c08f32a8b8086ef029e3060245dcff7a4a1
Dima Sheptytskyi <channektoshka@gmail.com>
2024-04-25 23:44:50 +0300

    feat: custom user model

--- hackathon_tesk_task_backend/hackathon_test_task/settings.py
+++ hackathon_tesk_task_backend/hackathon_test_task/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'advertisement'
+    'advertisement',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.CustomUserModel"
---
+++ hackathon_tesk_task_backend/users/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class UserTypeChoices(models.TextChoices):
+    needs = 'Потребує допомоги'
+    helper = 'Надає допомогу'
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUserModel(AbstractBaseUser, PermissionsMixin):
+    USERNAME_FIELD = "email"
+
+    email = models.EmailField(verbose_name='Електрона пошта', unique=True, db_index=True)
+    first_name = models.CharField(verbose_name='Імʼя')
+    last_name = models.CharField(verbose_name='Прізвище')
+    user_type = models.CharField(verbose_name='Тип користувача', choices=UserTypeChoices.choices, max_length=99)
+    is_staff = models.BooleanField(verbose_name='Персонал', default=False)
+    is_active = models.BooleanField(verbose_name='Активний', default=True)
+    date_joined = models.DateTimeField(verbose_name='Дата приєднання', default=timezone.now)
+
+    objects = CustomUserManager()
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'Користувач'
+        verbose_name_plural = 'Користувачі'
+
+    def __str__(self) -> str:
+        return self.email
