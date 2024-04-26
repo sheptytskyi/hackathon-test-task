793bba534c103b3360a871f0ac8336c598f689f2
truskavka <kozachuk.dz@gmail.com>
2024-04-26 23:37:14 +0300

    Advertisement and Category models added

--- hackathon_tesk_task_backend/advertisement/models.py
+++ hackathon_tesk_task_backend/advertisement/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from ..users.models import CustomUserModel
+from datetime import timedelta
+from django.utils import timezone
+
+
+def tomorrow():
+    return timezone.now() + timedelta(days=1)
+
+
+class StatusChoices(models.TextChoices):
+    active = 'active', 'Активне'
+    closed = 'closed', "Закрите"
+    completed = 'completed', 'Виконане'
+
+
+class PriorityChoices(models.TextChoices):
+    moderate = 'moderate', 'Помірний'
+    high = 'high', 'Високий'
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="Назва категорії", unique=True, max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Advertisement(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.OneToOneField(CustomUserModel, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="Заголовок", max_length=99)
+    description = models.CharField(verbose_name="Опис")
+    categories = models.ManyToManyField(Category)
+    location = models.CharField(verbose_name="Місцезнаходження", max_length=99)
+    picture = models.ImageField(upload_to=f'advertisement_pictures/{datetime.today()}/{id}', null=True)
+    status = models.CharField(verbose_name="Статус", choices=StatusChoices.choices, max_length=99)
+    time_validity = models.DateTimeField(verbose_name="Виконати до", default=tomorrow)
+    priority = models.CharField(verbose_name="Статус", choices=PriorityChoices.choices, max_length=99)
+
+    class Meta:
+        db_table = 'advertisements'
+        verbose_name = 'Оголошення'
+        verbose_name_plural = 'Оголошення'
+
+    def __str__(self) -> str:
+        return str(self.id)
--- hackathon_tesk_task_backend/hackathon_test_task/settings.py
+++ hackathon_tesk_task_backend/hackathon_test_task/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.CustomUserModel"
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
