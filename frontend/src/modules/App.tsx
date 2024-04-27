92ebe29070156b75d17fd5b98a922d9c3e05c1da
Volodymyr Melnyk <fron10der@gmail.com>
2024-04-27 16:32:52 +0300

    Main Layout: removed useProfile()

--- frontend/src/modules/App.tsx
+++ frontend/src/modules/App.tsx
@@ -1,15 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { RouterSettings } from '@router';
 import { Loader } from '@/ui';
-import useProfile from '@hooks/useProfile.ts';
 import { useAppSelector } from '@hooks/store.ts';
 import { selectLoadingIds } from '@app/slices/ui.ts';
 
 const App = () => {
   const loadingsId = useAppSelector(selectLoadingIds);
-
-  // TODO remove after CORS fix
-  useProfile();
 
   return (
     <>
